fcf27d33db2b61acb6a035fef0bf1d02e1f1d9a9
Noman Ali <noman710@protonmail.com>
2022-01-07 11:02:11 -0500

    late commit, added event handler to pause the website from redirecting, similar to what was done in lecture

--- script/script.js
+++ script/script.js
@@ -3,12 +3,14 @@ let check = () =>{
     //checking if the password is 12345678
     const header = document.querySelector("h1")
     const password = document.getElementById("exampleInputPassword1").value
+    const form = document.querySelector("form")
     if(String(password) === "12345678"){
-        alert("Correct")
-        header.innerText = "Correct"
+        form.addEventListener("submit", (event) =>{
+            event.preventDefault()
+            header.innerText = "Correct"
+        })
     }
     else{
-        console.log(password)
         alert("Wrong password")
     }
 }
